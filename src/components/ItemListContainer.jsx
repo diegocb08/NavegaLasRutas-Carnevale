c418933f69343e2833cd68b3a03bacc3bdd5e938
diegocb08 <diegocb08@gmail.com>
2025-06-15 17:20:59 +0200

    pruebas en generacion del listado de productos con .map()

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -24,9 +24,13 @@ function ItemListContainer() {
   }, []);
 
   return (
-    <div>
+    <div className="itemListContainer">
       {items.map((item) => (
-        <p key={item.id}>{item.title}</p>
+        <div key={item.id} className="itemCard">
+          <img src={item.images[0]} alt={"item.title"} />
+          <p>{item.title}</p>
+          <p>Precio: ${item.price}</p>
+        </div>
       ))}
     </div>
   );
